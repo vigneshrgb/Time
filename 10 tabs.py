0ad10f3f6c4000af9da2ea48ce3224e0fa995ced
vigneshrgb <60288495+vigneshrgb@users.noreply.github.com>
2020-01-25 16:44:00 +0530

    Create 10 tabs.py

---
+++ 10 tabs.py
@@ -0,0 +1,11 @@
+import webbrowser
+webbrowser.open("https://en.wikipedia.org/wiki/Portal:Current_events")
+webbrowser.open("https://www.google.com")
+webbrowser.open("https://www.google.co.in")
+webbrowser.open("https://www.google.it")
+webbrowser.open("https://www.google.fr")
+webbrowser.open("https://www.abc.xyz")
+webbrowser.open("https://www.wow.com")
+webbrowser.open("https://www.aol.com")
+webbrowser.open("https://en.wikipedia.org/wiki/Special:Random")
+webbrowser.open("https://www.mayam.cc")
