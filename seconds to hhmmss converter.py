24e6e6796fbad2a6d0338b207f93937168942f62
vigneshrgb <60288495+vigneshrgb@users.noreply.github.com>
2020-01-29 09:13:23 +0530

    Update seconds to hhmmss converter.py

--- seconds to hhmmss converter.py
+++ seconds to hhmmss converter.py
@@ -1,5 +1,6 @@
 seconds = int(input("Enter seconds: "))
 ss = seconds % 60
 mm = (seconds // 60) % 60
-hr = (seconds // 3600)
-print("HR:" + str(hr) + "   " + "MM:" + str(mm) + "   " + "SS:" + str(ss))
+hr = (seconds // 3600) % 24
+day = (seconds // 86400) #Technically incorrect - Leap seconds need to be added
+print("Day: " + str(day) + "   " + "HR:" + str(hr) + "   " + "MM:" + str(mm) + "   " + "SS:" + str(ss))
